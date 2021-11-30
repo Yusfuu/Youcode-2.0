de5e9c7e45b179ff37ed8e6b7dee5822ffdec086
YounessHassoune <yhassoune7@gmail.com>
2021-11-30 21:33:57 +0100

    adding admin and candidate classes

---
+++ src/classes/User.js
@@ -0,0 +1,8 @@
+class Candidate {
+  url = "http://localhost:5000/candidate/";
+  data;
+  login = async (email, password) => {};
+  register = async (email, password) => {};
+  logOut = () => {};
+  getCandidateList = () => {};
+}
