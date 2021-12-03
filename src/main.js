fd7ffa5beccaa98808c8a9d7211b384ad3f4e58f
YounessHassoune <yhassoune7@gmail.com>
2021-12-03 16:22:12 +0100

    adding download txt file

--- src/main.js
+++ src/main.js
@@ -124,8 +124,8 @@ if (pathname === "/admin.html") {
                         <div class="flex-shrink-0 h-10 w-10">
                           <img
                             class="h-10 w-10 rounded-full"
-                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
-                            alt=""
+                            src="https://picsum.photos/200/240"
+                            alt="avatar"
                           />
                         </div>
                         <div class="ml-4">
